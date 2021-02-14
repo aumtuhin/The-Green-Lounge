a255018a7a8a6e7a2a9a019c9a1cad6d9a06e3c3
aumtuhin <dev.aumtuhin@gmail.com>
2021-02-14 22:34:13 +0100

    crt-item cart dropdown and cart utils updated

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './cart-dropdown.styles.scss';
 import CustomButton from '../custom-button/custom-buttom.component';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
     return (
-        <div className="cart-dropdown">
-            <div className="cart-items">
-                <CustomButton>GO TO CHECKOUT</CustomButton>
+        <div className='cart-dropdown'>
+            <div className='cart-items'>
+                {cartItems.map(cartItem => (
+                    <CartItem key={cartItem.id} item={cartItem} />
+                ))}
             </div>
+            <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     );
 }
 
-export default CartDropdown;
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,15 +1,15 @@
-export const addItemToCart = (cartItems, cartItemsToAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(
-        cartItem => cartItem.id === cartItemsToAdd.id
+        cartItem => cartItem.id === cartItemToAdd.id
+    );
+
+    if (existingCartItem) {
+        return cartItems.map(cartItem =>
+            cartItem.id === cartItemToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
         );
+    }
 
-        if(existingCartItem) {
-            return cartItems.map(cartItem => 
-                cartItem.id === cartItemsToAdd.id ?
-                { ...cartItem, quantity: cartItem.quantity + 1 }
-                : { cartItem }
-                )
-        }
-
-        return [...cartItems, { ...cartItemsToAdd, quantity: 1 }];
-}
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
