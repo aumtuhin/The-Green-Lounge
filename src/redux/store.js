46135dd250fc5db0e83e9cc7cdc21c6b65381a2c
aumtuhin <dev.aumtuhin@gmail.com>
2021-02-18 13:56:01 +0100

    added redux persist package to store cart state to local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
-import { createStore, applyMiddleware} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
@@ -6,6 +7,7 @@ import rootReducer from './root-reducer';
 const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const persistor = persistStore(store);
 
 
-export default store;
+export { store, persistor };
