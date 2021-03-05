77ef4782f49eb523bfe9528afb6dbf49c8abb6a6
aumtuhin <dev.aumtuhin@gmail.com>
2021-03-05 05:55:38 +0100

    name changed

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,7 +14,7 @@ const Header = ({ currentUser }) => {
         <div className="menu fixed-top">
             <div className="container">
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <Link className="navbar-brand" to='/'><i className="fa fa-pagelines"></i> The Forest Lounge</Link>
+                    <Link className="navbar-brand" to='/'><i className="fa fa-pagelines"></i> The Green Lounge</Link>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
