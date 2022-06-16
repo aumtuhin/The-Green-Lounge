e3c46edcd7460e126d70d2502968ac21ef9f3a1e
ozair-dev <truemail785@gmail.com>
2022-06-16 20:53:22 +0500

    Fix responsiveness issues

--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import SignIn from '../../components/sign-in/sign-in.component';
-import SignUp from '../../components/sign-up/sign-up.component';
-import './signin.signup.syles.scss';
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
+import "./signin.signup.syles.scss";
 
 const SigninSignup = () => {
-    return (
-        <div className="auth-page container">
-            <div className="row">
-                <div className="col-md-6">
-                    <SignIn />
-                </div>
-                <div className="col-md-6">
-                    <SignUp />
-                </div>
-            </div>
+  return (
+    <div className="auth-page container">
+      <div className="row">
+        <div className="col-md-6">
+          <SignIn />
         </div>
-    );
-}
+        <div className="col-md-6 mt-4 mt-md-0">
+          <SignUp />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SigninSignup;
